91cc975b67b11d1f77d7f8f465ccbaac8cd1d6d4
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-17 14:40:20 -0500

    props and inheritance, sorta

--- components/src/index.js
+++ components/src/index.js
@@ -8,6 +8,13 @@ const App = () => {
     return (
         <div className="ui container comments">
             <ApprovalCard>
+                <div>
+                    <h4>WARNING!!!!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
               <CommentDetail 
                 author="Sam"  
                 timeAgo="Today at 4:45PM" 
@@ -17,23 +24,23 @@ const App = () => {
             </ApprovalCard>
 
             <ApprovalCard>
-            <CommentDetail 
-            author="Alex"  
-            timeAgo="Today at 2:00AM" 
-            content="Some Comment 2" 
-            image={faker.image.animals()} 
-            />
+                <CommentDetail 
+                author="Alex"  
+                timeAgo="Today at 2:00AM" 
+                content="Some Comment 2" 
+                image={faker.image.animals()} 
+                />
             </ApprovalCard>
 
             <ApprovalCard>
             <CommentDetail 
-            author="Jane"  
-            timeAgo="Yesterday at 5:00PM" 
-            content="Some Comment 3" 
-            image={faker.image.nature()} 
-            />
+                author="Jane"  
+                timeAgo="Yesterday at 5:00PM" 
+                content="Some Comment 3" 
+                image={faker.image.nature()} 
+                />
             </ApprovalCard>
-            
+
         </div>
     )
 };
