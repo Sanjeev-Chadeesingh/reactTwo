fdfa10d55664af5f47c7908ae3f75c646de411b9
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-17 13:20:15 -0500

    added props and reduced lines

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Alex" />
-            <CommentDetail author="Jane" />
+            <CommentDetail 
+            author="Sam"  
+            timeAgo="Today at 4:45PM" 
+            content="Some Comment 1" 
+            image={faker.image.people()} 
+            />
+            <CommentDetail 
+            author="Alex"  
+            timeAgo="Today at 2:00AM" 
+            content="Some Comment 2" 
+            image={faker.image.animals()} 
+            />
+            <CommentDetail 
+            author="Jane"  
+            timeAgo="Yesterday at 5:00PM" 
+            content="Some Comment 3" 
+            image={faker.image.nature()} 
+            />
         </div>
     )
 };
